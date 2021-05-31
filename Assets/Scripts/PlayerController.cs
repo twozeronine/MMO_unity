b6db223245bacab7a2d8cad91c3638d9660c0727
twozeronine <amdusias209@gmail.com>
2021-05-31 15:59:58 +0900

    PlayerController.cs / Vector3 직접 구현

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,12 +2,53 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// 1. 위치 벡터
+// 2. 방향 벡터
+struct MyVector
+{
+  public float x;
+  public float y;
+  public float z;
+
+  //          +
+  //     +    +
+  // +--------+
+  public float magnitude { get { return Mathf.Sqrt(x * x + y * y + z * z); } }
+  public MyVector normailzed { get { return new MyVector(x / magnitude, y / magnitude, z / magnitude); } }
+
+  public MyVector(float x, float y, float z) { this.x = x; this.y = y; this.z = z; }
+
+  public static MyVector operator +(MyVector a, MyVector b)
+  {
+    return new MyVector(a.x + b.x, a.y + b.y, a.z + b.z);
+  }
+  public static MyVector operator -(MyVector a, MyVector b)
+  {
+    return new MyVector(a.x - b.x, a.y - b.y, a.z - b.z);
+  }
+
+  public static MyVector operator *(MyVector a, float d)
+  {
+    return new MyVector(a.x * d, a.y * d, a.z * d);
+  }
+}
 public class PlayerController : MonoBehaviour
 {
   [SerializeField]
   float _speed = 10.0f;
   void Start()
   {
+    MyVector to = new MyVector(10.0f, 0.0f, 0.0f);
+    MyVector from = new MyVector(5.0f, 0.0f, 0.0f);
+    MyVector dir = to - from; // ( -5.0f, 0.0f , 0.0f);
+
+    dir = dir.normailzed; // (1.0f, 0.0f, 0.0f)
+
+    MyVector newPos = from + dir * _speed;
+
+    // 방향 벡터
+    //1. 거리 (크기)   5 magnitude
+    //2. 실제 방향    ->
 
   }
 
@@ -21,6 +62,7 @@ public class PlayerController : MonoBehaviour
 
     // World -> Local
     // InverseTransformDirection
+
 
     //tranform.Translate 플레이어가 바라보는 방향으로 계산해서 이동 
 
