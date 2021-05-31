80373d4a4e08218d698edd7f68e60d7499bc556d
twozeronine <amdusias209@gmail.com>
2021-05-31 13:51:11 +0900

    Managers / 싱글톤 패턴을 이용한 매니저 생성

--- Assets/Scripts/Managers/Managers.cs
+++ Assets/Scripts/Managers/Managers.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class Managers : MonoBehaviour
 {
-  // Start is called before the first frame update
+  static Managers s_instance; // 유일성이 보장된다
+  public static Managers Instance { get { init(); return s_instance; } } // 유일한 매니저를 갖고온다
+
   void Start()
   {
-    // 초기화
+    init();
   }
 
-  // Update is called once per frame
-  void Update()
+  public void hello()
   {
+    Debug.Log("hello");
+  }
 
+  static void init()
+  {
+    if (s_instance == null)
+    {
+      GameObject go = GameObject.Find("@Managers");
+      if (go == null)
+      {
+        go = new GameObject { name = "@Managers" };
+        go.AddComponent<Managers>();
+      }
+
+      DontDestroyOnLoad(go);
+      s_instance = go.GetComponent<Managers>();
+    }
   }
 }
---
+++ Assets/Scripts/Managers/Player.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+  // Start is called before the first frame update
+  void Start()
+  {
+    Managers mg = Managers.Instance;
+    Managers.Instance.hello();
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+}
