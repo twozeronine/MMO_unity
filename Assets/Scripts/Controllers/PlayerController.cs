f2509f800881cbc08380d5c3e7e3cd8f658bce89
twozeronine <amdusias209@gmail.com>
2021-06-01 15:35:14 +0900

    PlayerController.cs / state 패턴으로 리팩토링

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -36,81 +36,81 @@ public class PlayerController : MonoBehaviour
 {
   [SerializeField]
   float _speed = 10.0f;
-
-  bool _moveToDest = false;
   Vector3 _destPos;
+
+
+
   void Start()
   {
 
-    // 실수로 다른 곳에서 OnKeyboard를 이미 등록했다면 두번 등록이 되기 때문에 그것을 방지하기 위하여 한번 빼고 시작하는것이다.
-    Managers.Input.KeyAction -= OnKeyboard;
-    Managers.Input.KeyAction += OnKeyboard;
+    // 실수로 다른 곳에서 Action을 이미 등록했다면 두번 등록이 되기 때문에 그것을 방지하기 위하여 한번 빼고 시작하는것이다.
     Managers.Input.MouseAction -= OnMouseClicked;
     Managers.Input.MouseAction += OnMouseClicked;
   }
 
-  void Update()
+  float wait_run_ratio = 0;
+  public enum PlayerState
   {
-    if (_moveToDest)
-    {
-      Vector3 dir = _destPos - transform.position;
-      // 도착
-      if (dir.magnitude < 0.0001f)
-      {
-        _moveToDest = false;
-      }
-      else
-      {
-        float moveDist = Mathf.Clamp(_speed * Time.deltaTime, 0, dir.magnitude);
-        transform.position += dir.normalized * moveDist;
+    Die,
+    Moving,
+    Idle,
+  }
 
-        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(dir), 20 * Time.deltaTime);
-      }
-    }
 
-    Animator anim = GetComponent<Animator>();
-    if (_moveToDest)
+  PlayerState _state = PlayerState.Idle;
+  void UpdateDie()
+  {
+    // 아무것도 못함
+  }
+  void UpdateMoving()
+  {
+    Vector3 dir = _destPos - transform.position;
+    // 도착
+    if (dir.magnitude < 0.0001f)
     {
-      anim.Play("RUN");
+      _state = PlayerState.Idle;
     }
     else
     {
-      anim.Play("WAIT");
+      float moveDist = Mathf.Clamp(_speed * Time.deltaTime, 0, dir.magnitude);
+      transform.position += dir.normalized * moveDist;
+
+      transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(dir), 20 * Time.deltaTime);
     }
 
+    // 애니메이션
+    wait_run_ratio = Mathf.Lerp(wait_run_ratio, 1, 10.0f * Time.deltaTime);
+    Animator anim = GetComponent<Animator>();
+    anim.SetFloat("wait_run_ratio", wait_run_ratio);
+    anim.Play("WAIT_RUN");
+  }
+  void UpdateIdle()
+  {
+    wait_run_ratio = Mathf.Lerp(wait_run_ratio, 0, 10.0f * Time.deltaTime);
+    Animator anim = GetComponent<Animator>();
+    anim.SetFloat("wait_run_ratio", wait_run_ratio);
+    anim.Play("WAIT_RUN");
   }
 
-  void OnKeyboard()
+  void Update()
   {
-    if (Input.GetKey(KeyCode.W))
+    switch (_state)
     {
-      transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.forward), 0.2f);
-      transform.position += Vector3.forward * Time.deltaTime * _speed;
+      case PlayerState.Die:
+        UpdateDie();
+        break;
+      case PlayerState.Moving:
+        UpdateMoving();
+        break;
+      case PlayerState.Idle:
+        UpdateIdle();
+        break;
     }
-    if (Input.GetKey(KeyCode.S))
-    {
-
-      transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.back), 0.2f);
-      transform.position += Vector3.back * Time.deltaTime * _speed;
-    }
-    if (Input.GetKey(KeyCode.A))
-    {
-
-      transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.left), 0.2f);
-      transform.position += Vector3.left * Time.deltaTime * _speed;
-    }
-    if (Input.GetKey(KeyCode.D))
-    {
-      transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.right), 0.2f);
-      transform.position += Vector3.right * Time.deltaTime * _speed;
-    }
-
-    _moveToDest = false;
   }
 
   void OnMouseClicked(Define.MouseEvent evt)
   {
-
+    if (_state == PlayerState.Die) return;
     Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
     Debug.DrawRay(Camera.main.transform.position, ray.direction * 100.0f, Color.red, 1.0f);
 
@@ -120,7 +120,7 @@ public class PlayerController : MonoBehaviour
     if (Physics.Raycast(ray, out hit, 100.0f, mask))
     {
       _destPos = hit.point;
-      _moveToDest = true;
+      _state = PlayerState.Moving;
       //Debug.Log($"Raycast Camera @ {hit.collider.gameObject.name}");
     }
   }
