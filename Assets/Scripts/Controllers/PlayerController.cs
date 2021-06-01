9ecc024cf3ceb501253c706f49d8359783fc17d8
twozeronine <amdusias209@gmail.com>
2021-06-01 13:45:35 +0900

    PlayerController.cs / 화면 클릭시 플레이어 이동 구현

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -36,12 +36,36 @@ public class PlayerController : MonoBehaviour
 {
   [SerializeField]
   float _speed = 10.0f;
+
+  bool _moveToDest = false;
+  Vector3 _destPos;
   void Start()
   {
 
     // 실수로 다른 곳에서 OnKeyboard를 이미 등록했다면 두번 등록이 되기 때문에 그것을 방지하기 위하여 한번 빼고 시작하는것이다.
     Managers.Input.KeyAction -= OnKeyboard;
     Managers.Input.KeyAction += OnKeyboard;
+    Managers.Input.MouseAction -= OnMouseClicked;
+    Managers.Input.MouseAction += OnMouseClicked;
+  }
+
+  void Update()
+  {
+    if (_moveToDest)
+    {
+      Vector3 dir = _destPos - transform.position;
+      // 도착
+      if (dir.magnitude < 0.0001f)
+      {
+        _moveToDest = false;
+      }
+      else
+      {
+        float moveDist = Mathf.Clamp(_speed * Time.deltaTime, 0, dir.magnitude);
+        transform.position += dir.normalized * moveDist;
+        transform.LookAt(_destPos);
+      }
+    }
   }
 
   void OnKeyboard()
@@ -68,5 +92,27 @@ public class PlayerController : MonoBehaviour
       transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.right), 0.2f);
       transform.position += Vector3.right * Time.deltaTime * _speed;
     }
+
+    _moveToDest = false;
   }
+
+  void OnMouseClicked(Define.MouseEvent evt)
+  {
+    if (evt != Define.MouseEvent.Click)
+      return;
+
+    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+    Debug.DrawRay(Camera.main.transform.position, ray.direction * 100.0f, Color.red, 1.0f);
+
+    LayerMask mask = LayerMask.GetMask("Wall");
+
+    RaycastHit hit;
+    if (Physics.Raycast(ray, out hit, 100.0f, mask))
+    {
+      _destPos = hit.point;
+      _moveToDest = true;
+      //Debug.Log($"Raycast Camera @ {hit.collider.gameObject.name}");
+    }
+  }
+
 }
--- Assets/Scripts/Utils/Define.cs
+++ Assets/Scripts/Utils/Define.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class Define
 {
+  public enum MouseEvent
+  {
+    Press,
+    Click,
+  }
   public enum CameraMode
   {
     QuaterView,
